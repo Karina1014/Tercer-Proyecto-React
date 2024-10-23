bb309232d9f389c36501dd18a6cda7ae6974b848
Karina1014 <janneth_wi15@hotmail.com>
2024-10-22 19:15:23 -0500

    💻 Realizado con exito

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Aline from '/src/assets/logo.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='main container'>
+        <img
+        className='logo-image'
+        src={Aline}
+        alt='logo-Aline'
+        />
+      </div>
     </div>
   );
 }
